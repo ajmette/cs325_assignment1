8f09cff5c9c2ef19606b77d078bbe6f0c3f78e5c
ajmette <amette@siue.edu>
2024-09-10 16:06:47 -0500

    adding program1.py

---
+++ program1.py
@@ -0,0 +1,10 @@
+print("Input four integers: ")
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+sum = num1 + num2 + num3 + num4
+
+print("The sum of your four numbers is: ", sum)
